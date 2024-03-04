38d8507f409bde1cd050b7632dd46b4c3b25d401
Abraão Silva de Araújo <121314336+Abraaodearaujo@users.noreply.github.com>
2024-03-04 10:45:20 -0300

    atualizaçao

--- js/antes-login/pagina-inicial.js
+++ js/antes-login/pagina-inicial.js
@@ -2,17 +2,18 @@ document.getElementById("vamos-la").addEventListener("click", function () {
     var emailInput = document.getElementById("email").value;
 let emailValido = document.getElementById("email-valido");
 let emailVazio = document.getElementById("email-vazio");
+let placeholder = document.getElementById("email-pla")
 
     if (emailInput.trim() !== "") {
         var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         if (emailPattern.test(emailInput)) {
             window.location.href = "../../html/antes-login/pagina-cadastro.html";
         } else {
-            emailValido.style.display = "block";
+            alert("Digite um email valido")
             setTimeout(function() {emailValido.style.display = "none"}, 3000);
         }   
     } else {
-        emailVazio.style.display = "block";
+        alert("Digite um email valido")
         setTimeout(function() {emailVazio.style.display = "none"}, 3000);
     }
 });
