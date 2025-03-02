422838b2a15abd4a5c686084c3006aad4031b2aa
Abraão Silva de Araújo <121314336+Abraaodearaujo@users.noreply.github.com>
2025-03-02 05:44:32 -0300

    consertando a escolha de perfil
    estava com alguns bugs de proporção mas já foi resolvido

---
+++ js/depois-login/quem-esta.js
@@ -0,0 +1,15 @@
+document.getElementById("ana").addEventListener("click", function () {
+    window.location.href = "../../html/depois-login/tela-principal.html";
+})
+document.getElementById("aderbal").addEventListener("click", function () {
+    window.location.href = "../../html/depois-login/tela-principal.html";
+})
+document.getElementById("eliana").addEventListener("click", function () {
+    window.location.href = "../../html/depois-login/tela-principal.html";
+})
+document.getElementById("ivano").addEventListener("click", function () {
+    window.location.href = "../../html/depois-login/tela-principal.html";
+})
+document.getElementById("abraao").addEventListener("click", function () {
+    window.location.href = "../../html/depois-login/tela-principal.html";
+})
