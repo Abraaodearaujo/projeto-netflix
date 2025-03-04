052f6826f03138f88d3f5eb43b614a1e6c497f3c
Abraão Silva de Araújo <121314336+Abraaodearaujo@users.noreply.github.com>
2025-03-04 06:12:14 -0300

    ajustando os perfis e os idiomas

--- js/depois-login/quem-esta.js
+++ js/depois-login/quem-esta.js
@@ -1,15 +1,15 @@
 document.getElementById("ana").addEventListener("click", function () {
-    window.location.href = "../../html/depois-login/tela-principal.html";
+    window.location.href = "../../html/depois-login/tela-principal-amarelo.html";
 })
 document.getElementById("aderbal").addEventListener("click", function () {
-    window.location.href = "../../html/depois-login/tela-principal.html";
+    window.location.href = "../../html/depois-login/tela-principal-azul.html";
 })
 document.getElementById("eliana").addEventListener("click", function () {
-    window.location.href = "../../html/depois-login/tela-principal.html";
+    window.location.href = "../../html/depois-login/tela-principal-panda.html";
 })
 document.getElementById("ivano").addEventListener("click", function () {
-    window.location.href = "../../html/depois-login/tela-principal.html";
+    window.location.href = "../../html/depois-login/tela-principal-verde.html";
 })
 document.getElementById("abraao").addEventListener("click", function () {
-    window.location.href = "../../html/depois-login/tela-principal.html";
+    window.location.href = "../../html/depois-login/tela-principal-galo.html";
 })
